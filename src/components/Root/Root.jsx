4e1485b097e99ac685167f9ca1543854feb48969
Abdur Razzak <abdurrazzakyeamin@gmail.com>
2024-11-01 00:38:02 +0600

    added to github

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
+
+
+const Root = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
